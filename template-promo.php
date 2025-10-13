fdc4099fbae50dd81a1be8b2721004967b44b373
Roman Vasilyev <vasilyev-r@mail.ru>
2025-10-13 10:31:40 +0300

    Add new counter

--- template-promo.php
+++ template-promo.php
@@ -59,6 +59,19 @@ if (isset($_SESSION['win'])) {
 		</div>
 	</noscript>
 	<!-- /Yandex.Metrika counter -->
+	<!-- Yandex.Metrika counter -->
+	<script type="text/javascript">
+		(function(m,e,t,r,i,k,a){
+			m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+			m[i].l=1*new Date();
+			for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+			k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+		})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104613973', 'ym');
+
+		ym(104613973, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+	</script>
+	<noscript><div><img src="https://mc.yandex.ru/watch/104613973" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	<!-- /Yandex.Metrika counter -->
 	<style>
 		.gallery-grid__item img,
 		.gallery__item img {
